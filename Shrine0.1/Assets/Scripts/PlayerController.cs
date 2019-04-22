56b9ac1b7f2b2f635aa2e78f198ca9539ceacb7b
khalen <khalenstensby@gmail.com>
2019-04-21 22:25:58 -0700

    working on player spawning

--- Shrine0.1/Assets/Scripts/PlayerController.cs
+++ Shrine0.1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     private Animator anim;
+    private Rigidbody2D myRigidBody;
+
     private bool isMoving;
     private Vector2 lastMove;
 
@@ -13,6 +15,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        DontDestroyOnLoad(transform.gameObject);
     }
 
     // Update is called once per frame
@@ -20,28 +25,32 @@ public class PlayerController : MonoBehaviour
     {
         isMoving = false;
 
-        float moveHorizontal = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
-        float moveVertical = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        float moveHorizontal = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float moveVertical = Input.GetAxisRaw("Vertical") * moveSpeed;
 
         if(moveVertical != 0f && moveHorizontal != 0f)
         {
             moveHorizontal *= 0.7071f;
             moveVertical *= 0.7071f;
             isMoving = true;
-            transform.Translate(new Vector3(moveHorizontal, moveVertical, 0f));
+            myRigidBody.velocity = new Vector2(moveHorizontal, moveVertical);
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
         if(moveHorizontal != 0f && moveVertical == 0f)
         {
             isMoving = true;
-            transform.Translate(new Vector3(moveHorizontal, 0f, 0f));
+            myRigidBody.velocity = new Vector2(moveHorizontal, moveVertical);
             lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
         if(moveVertical != 0f && moveHorizontal == 0f)
         {
             isMoving = true;
-            transform.Translate(new Vector3(0f, moveVertical, 0f));
+            myRigidBody.velocity = new Vector2(moveHorizontal, moveVertical);
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+        if(moveHorizontal == 0 && moveVertical == 0)
+        {
+            myRigidBody.velocity = new Vector2(0f, 0f);
         }
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
